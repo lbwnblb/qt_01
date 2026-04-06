3a243423f01b8a10a799fa27223368bc52d28756
Your Name <you@example.com>
2026-04-06 16:22:29 +0800

    1

---
+++ qt_01/qt_01.cpp
@@ -0,0 +1,18 @@
+﻿#include <QApplication>
+#include <QPushButton>
+#include <QMessageBox>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    QPushButton button("Click Me!");
+    button.setFixedSize(200, 60);
+
+    QObject::connect(&button, &QPushButton::clicked, [&]() {
+        QMessageBox::information(&button, "Hello", "You clicked the button!");
+        });
+
+    button.show();
+    return app.exec();
+}
